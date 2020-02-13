34cbd75628920a9d275e49432399137ce8b3d0f5
golden_silence <13512835407@163.com>
2020-02-13 20:44:56 +0800

    springboot cache test

--- src/main/java/com/haohua/demo/DemoApplication.java
+++ src/main/java/com/haohua/demo/DemoApplication.java
@@ -2,12 +2,18 @@ package com.haohua.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableCaching
+@CacheConfig(cacheNames = {"cashTest"})
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -15,8 +21,20 @@ public class DemoApplication {
     }
 
     @GetMapping("/hello/{name}")
+    @CachePut(key = "guest")
     public String hello(@PathVariable String name) {
-        return "hi  welcome" + name + " alredy done";
+        return  name;
+    }
+
+    @GetMapping("/hello/again/{param}")
+    @Cacheable(key = "guest")
+    public String helloAgain(@PathVariable String param){
+        return param;
+    }
+
+    @GetMapping("/hello/del")
+    @Cacheable(key = "guest")
+    public void helloDel(){
     }
 
 }
