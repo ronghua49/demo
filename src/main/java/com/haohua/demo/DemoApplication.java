9092aea3224561c37c8f3f9caa0b3d3aafaa16e6
golden_silence <13512835407@163.com>
2020-02-13 12:49:38 +0800

    1

--- src/main/java/com/haohua/demo/DemoApplication.java
+++ src/main/java/com/haohua/demo/DemoApplication.java
@@ -16,7 +16,7 @@ public class DemoApplication {
 
     @GetMapping("/hello/{name}")
     public String hello(@PathVariable String name) {
-        return "hello1  welcome  " + name;
+        return "hello2  welcome  " + name;
     }
 
 }
